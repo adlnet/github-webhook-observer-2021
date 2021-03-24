74b6e55f29971b92f1d782625434c70b4b504c95
TreyH <vbhayden3@gmail.com>
2021-03-24 12:10:27 -0400

    adding branch patterns

--- watch.js
+++ watch.js
@@ -14,6 +14,46 @@ const { execSync } = require('child_process');
 const dotenv = require("dotenv");
 
 /***
+ * Repo config
+ **/
+const dotenvConfig = dotenv.config();
+const args = process.argv;
+const repo = args[2] || process.env.REPO || "../path-to-project-folder";
+const secret = args[3] || process.env.WEBHOOK_SECRET || "git-observer-secret";
+const port = args[4] || process.env.PORT || 8000;
+const rebuildCommand = args[5] || process.env.REBUILD_COMMAND || "bash rebuild.sh"
+const user = args[6] || process.env.PULLING_USER || "ubuntu";
+const branchPattern = args[7] || process.env.BRANCH_PATTERN || null;
+
+/**
+ * Check whether or not a given string matches a given wildcard pattern.  
+ * 
+ * Stolen from: https://stackoverflow.com/a/32402438
+ * 
+ * @param {String} str The string we want to compare. 
+ * @param {String} rule The wildcard pattern to check against.
+ * @returns 
+ */
+const wildcardMatch = (str, rule) => {
+    // for this solution to work on any string, no matter what characters it has
+    var escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+
+    // "."  => Find a single character, except newline or line terminator
+    // ".*" => Matches any string that contains zero or more characters
+    rule = rule.split("*").map(escapeRegex).join(".*");
+
+    // "^"  => Matches any string with the following at the beginning of it
+    // "$"  => Matches any string with that in front at the end of it
+    rule = "^" + rule + "$"
+
+    //Create a regular expression object for matching string
+    var regex = new RegExp(rule);
+
+    //Returns true if it finds a match, otherwise it returns false
+    return regex.test(str);
+}
+
+/***
  * Use the file system to determine which containers the modified files belong to.
  * Args: 1. Array [] of containerNames from getContainerNames 2. Array [] of strings of modified files from github req body 
  * Return: Array []
@@ -36,16 +76,6 @@ const whichContainersRebuild = (composeConfig, modifiedFiles) => {
 
 const isEqualLength = (x, y) => x.length == y.length;
 
-/***
- * Repo config
- **/
-const dotenvConfig = dotenv.config();
-const args = process.argv;
-const repo = args[2] || process.env.REPO || "../path-to-project-folder";
-const secret = args[3] || process.env.WEBHOOK_SECRET || "git-observer-secret";
-const port = args[4] || process.env.PORT || 8000;
-const rebuildCommand = args[5] || process.env.REBUILD_COMMAND || "bash rebuild.sh"
-const user = args[6] || process.env.PULLING_USER || "ubuntu";
 
 http.createServer(function (req, res) {
 
@@ -66,6 +96,14 @@ http.createServer(function (req, res) {
 
             //Github headers list the files(including full path) changed from last commit in array of strings.
             let body = JSON.parse(chunk);
+
+            // Only do this if we're actually monitoring this branch, or if there wasn't a branch
+            // specified at all
+            let branch = body.ref.substr("refs/heads/".length);
+            let ignoreThisBranch = branchPattern != null && !wildcardMatch(branch, branchPattern);
+            if (ignoreThisBranch)
+                return;
+
             let filesModifed = body['head_commit']['modified'] || [];
             let composeConfig = {};
 
